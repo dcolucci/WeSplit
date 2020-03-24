0cd75f0403c25681e01284398a95b6c14cdbb860
David Colucci <david_colucci@condenast.com>
2020-03-23 21:03:29 -0400

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Colucci, David on 3/23/20.
+//  Copyright © 2020 Colucci, David. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
